1c26a1b3e5771a42dab666e28a83ed884399063e
Daniel Lapido Martínez <100417214@alumnos.uc3m.es>
2020-03-25 18:48:52 +0100

    World map

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import dash
 import dash_bootstrap_components as dbc  # import the library
 import dash_core_components as dcc
 import dash_html_components as html
+import folium
 import dash_table
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
-
+import json
+from textwrap import dedent as d
 import pandas as pd
 import time
 import numpy as np
-import json
+
 
 # Read data
 Confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
@@ -21,8 +23,10 @@ Recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-
 
 report = pd.concat([Confirmed.iloc[:, [0, 1, 2, 3, -1]], Deaths.iloc[:, -1], Recovered.iloc[:, -1]], axis=1)
 report.loc[:, "Province/State"] = report.loc[:, "Province/State"].fillna("")
-report.columns.values[[4, 5, 6]] = ["Confirmed", "Deaths", "Recovered"]
 
+report.columns = ['Province/State', 'Country/Region', 'Lat', 'Long', "Confirmed", "Deaths", "Recovered"]
+
+report.loc[:, "Confirmed"] = report.loc[:, "Confirmed"].fillna(0)
 date = pd.to_datetime(Confirmed.columns[4:-1])
 
 conf2 = pd.melt(Confirmed, id_vars=Confirmed.columns[0:4])
@@ -33,7 +37,7 @@ deaths2 = pd.melt(Deaths, id_vars=Deaths.columns[0:4])
 recovered2 = pd.melt(Recovered, id_vars=Recovered.columns[0:4])
 
 globalreport = pd.concat([conf2, deaths2.value, recovered2.value], axis=1)
-globalreport.columns.values[[4, 5, 6, 7]] = ["Date", "Confirmed", "Deaths", "Recovered"]
+globalreport.columns = ['Province/State', 'Country/Region', 'Lat', 'Long', 'Date', 'Confirmed', 'Deaths', 'Recovered']
 
 globalreport['Country/Region'] = pd.Categorical(globalreport['Country/Region'])
 # print(globalreport.dtypes)
@@ -42,12 +46,45 @@ factors = sorted(globalreport['Country/Region'].unique())
 
 
 
+
+import pandas as pd
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+
+import plotly.express as px
+
+# fig = px.scatter_mapbox(report, lat="Lat", lon="Long", hover_name="Province/State", hover_data=["Country/Region", "Confirmed"],
+#                         color_discrete_sequence=["fuchsia"], zoom=1, height=800)
+
+bubble = np.log2(report.Confirmed + 1)
+
+fig = px.scatter_geo(report, lat="Lat", lon="Long",
+                     hover_name="Country/Region", size=bubble, height=800, projection="orthographic")
+
+# m = folium.Map(location=[0, 0], tiles='cartodbpositron',
+#                min_zoom=1, max_zoom=4, zoom_start=1)
+#
+# for i in range(0, len(report)):
+#     folium.Circle(
+#         location=[report.iloc[i]['Lat'], report.iloc[i]['Long']],
+#         color='crimson',
+#         tooltip='<li><bold>Country : '+str(report.iloc[i]['Country/Region'])+
+#                     '<li><bold>Province : '+str(report.iloc[i]['Province/State'])+
+#                     '<li><bold>Confirmed : '+str(report.iloc[i]['Confirmed'])+
+#                     '<li><bold>Deaths : '+str(report.iloc[i]['Deaths'])+
+#                     '<li><bold>Recovered : '+str(report.iloc[i]['Recovered']),
+#         radius=int(report.iloc[i]['Confirmed'])**1.1).add_to(m)
+# m
+
+
+
 ################################################################################
 # APP
 ################################################################################
 
 app = dash.Dash()
-app.layout = html.H1("Covid-19")
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
 
 if __name__ == "__main__":
     app.run_server(port=5054, debug=True)
