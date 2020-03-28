33cedc0c292a3c62b086743b843f43c4ed910d96
Daniel Lapido Martínez <100417214@alumnos.uc3m.es>
2020-03-28 23:09:33 +0100

    Links changed

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash
 import dash_bootstrap_components as dbc  # import the library
 import dash_core_components as dcc
 import dash_html_components as html
@@ -14,12 +17,9 @@ import numpy as np
 
 
 # Read data
-Confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
-                        "/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-Deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
-                     "/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
-Recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
-                        "/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
+Confirmed = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-Confirmed_archived_0325.csv")
+Deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-Deaths_archived_0325.csv")
+Recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-Recovered_archived_0325.csv")
 
 report = pd.concat([Confirmed.iloc[:, [0, 1, 2, 3, -1]], Deaths.iloc[:, -1], Recovered.iloc[:, -1]], axis=1)
 report.loc[:, "Province/State"] = report.loc[:, "Province/State"].fillna("")
@@ -60,32 +60,52 @@ bubble = np.log2(report.Confirmed + 1)
 fig = px.scatter_geo(report, lat="Lat", lon="Long",
                      hover_name="Country/Region", size=bubble, height=800, projection="orthographic")
 
-# m = folium.Map(location=[0, 0], tiles='cartodbpositron',
-#                min_zoom=1, max_zoom=4, zoom_start=1)
-#
-# for i in range(0, len(report)):
-#     folium.Circle(
-#         location=[report.iloc[i]['Lat'], report.iloc[i]['Long']],
-#         color='crimson',
-#         tooltip='<li><bold>Country : '+str(report.iloc[i]['Country/Region'])+
-#                     '<li><bold>Province : '+str(report.iloc[i]['Province/State'])+
-#                     '<li><bold>Confirmed : '+str(report.iloc[i]['Confirmed'])+
-#                     '<li><bold>Deaths : '+str(report.iloc[i]['Deaths'])+
-#                     '<li><bold>Recovered : '+str(report.iloc[i]['Recovered']),
-#         radius=int(report.iloc[i]['Confirmed'])**1.1).add_to(m)
-# m
+fig2 = px.scatter_geo(report, lat="Lat", lon="Long",
+                     hover_name="Country/Region", size=bubble, height=800)
+
+fig3 = px.scatter_geo(report, lat="Lat", lon="Long",
+                     hover_name="Country/Region", size=bubble, height=400, projection="orthographic")
+
+m = folium.Map(location=[0, 0], tiles='cartodbpositron',
+               min_zoom=1, max_zoom=4, zoom_start=1)
+
+for i in range(0, len(report)):
+    folium.Circle(
+        location=[report.iloc[i]['Lat'], report.iloc[i]['Long']],
+        color='crimson',
+        tooltip='<li><bold>Country : '+str(report.iloc[i]['Country/Region'])+
+                    '<li><bold>Province : '+str(report.iloc[i]['Province/State'])+
+                    '<li><bold>Confirmed : '+str(report.iloc[i]['Confirmed'])+
+                    '<li><bold>Deaths : '+str(report.iloc[i]['Deaths'])+
+                    '<li><bold>Recovered : '+str(report.iloc[i]['Recovered']),
+        radius=int(report.iloc[i]['Confirmed'])**1.1).add_to(m)
 
 
+m.save('map.html')
 
-################################################################################
-# APP
-################################################################################
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash()
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    dcc.Tabs([
+        dcc.Tab(label='Tab one', children=[
+            dcc.Graph(
+                figure=fig
+            )
+        ]),
+        dcc.Tab(label='Tab two', children=[
+            dcc.Graph(
+                figure=fig2
+            )
+        ]),
+        dcc.Tab(label='Tab three', children=[
+            html.Iframe(id="map", srcDoc=open('map.html', 'r').read(), width='100%', height='300px')
+        ]),
+    ])
 ])
 
-if __name__ == "__main__":
-    app.run_server(port=5054, debug=True)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
